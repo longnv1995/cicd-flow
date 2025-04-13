c390b6db9cecb78826ea71d0e44749406d3f14fc
longn16 <lnguyen@deputy.com>
2025-04-13 14:50:53 +0700

    init proj

---
+++ tests/example.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('UI test example', {
+  tag: ['@smoke', '@ui']
+}, async ({ page }) => {
+  await page.goto('/demo-site');
+  await expect(page.getByTestId('heading_pg')).toHaveText('Vite + React');
+});
+
+test('API test example', {
+  tag: ['@api']
+}, async ({ page }) => {
+  await page.goto('/demo-site');
+  await expect(page.getByTestId('count_btn')).toContainText("0");
+});
+
+test('Visual test example', {
+  tag: ['@smoke', '@visual']
+}, async ({ page }) => {
+  await page.goto('/demo-site');
+  await expect(page.getByTestId('count_btn')).toContainText("0");
+});
