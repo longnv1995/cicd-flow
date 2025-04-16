2fbe67c92407437c8f21186c5cd7db2d3c8f1539
longn16 <lnguyen@deputy.com>
2025-04-16 16:28:15 +0700

    up

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -4,7 +4,8 @@ test.describe('Visual tests', async () => {
   test('Visual test example', {
     tag: ['@smoke', '@visual']
   }, async ({ page }, testInfo) => {
-    console.log(testInfo.project.testIgnore);
+    console.log(testInfo.project.name);
+    // console.log(suite.allTests());
     await page.goto('/demo-site');
     await expect(page.getByTestId('count_btn')).toContainText("0");
   });
